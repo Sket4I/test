11440f27b6b344e8348e75a14b08e0910e1d00bf
Sket4I <itseplyaev@gmail.com>
2020-03-21 20:36:59 +0300

    Create git.java

---
+++ testpoject/git.java
@@ -0,0 +1,15 @@
+package testpoject;
+
+import java.io.File;
+
+public class git {
+
+	public static void main(String[] args) {
+		File f=new File ("C:/Windows");
+		
+		
+		System.out.println("Hello-"+f.getTotalSpace());
+
+	}
+
+}
