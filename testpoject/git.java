10fff563b8c12acc6a9e7807b772ec6a10715c9b
Sket4I <itseplyaev@gmail.com>
2020-03-22 17:27:49 +0300

    1

--- testpoject/git.java
+++ testpoject/git.java
@@ -1,15 +1,15 @@
 package testpoject;
 
-import java.io.File;
+import java.io.*;
 
 public class git {
 
-	public static void main(String[] args) {
-		File f=new File ("C:/Windows");
-		
-		
-		System.out.println("Hello-"+f.getTotalSpace());
-
-	}
-
-}
+	public static void main(String[] args) throws Exception {
+			File file = new File("C:/Users/Slava/GitHub/test/test.txt");
+			FileReader fr = new FileReader(file);
+		      char [] a = new char[1000];
+		      fr.read(a);
+		         System.out.print(a);
+		      fr.close();
+		   }
+		}
