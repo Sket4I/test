e0c99dabbed5fc33ed7e440094852677b7dcd9da
Sket4I <itseplyaev@gmail.com>
2020-03-29 20:00:35 +0300

    Update git.java

--- testpoject/git.java
+++ testpoject/git.java
@@ -6,17 +6,26 @@ public class git {
 	public static void main(String[] args) {
 		try {
 			File file = new File("C:/Users/Slava/GitHub/test/test.txt");
-			FileReader fileReader = new FileReader(file);
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(new FileInputStream(file), "UTF-8"));
 
 			String line;
-			while((line = bufferedReader.readLine()) != null) {
+			while ((line = bufferedReader.readLine()) != null) {
 				System.out.println(line);
 			}
 
 			bufferedReader.close();
 		} catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("Error");
 		}
+		try {
+			Method1();
+		} catch (Exception e) {
+			System.out.println("RuntimeEXception"+e.getMessage());
+		}
+	}
+
+	public static void Method1() {
+		throw new RuntimeException("Runtime");
 	}
 }
